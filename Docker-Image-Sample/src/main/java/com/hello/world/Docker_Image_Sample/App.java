f7a86a2ed9bd9067789bd8e2dae45f6ce82325bf
Prasanna012 <90754550+Prasanna012@users.noreply.github.com>
2021-10-08 15:04:05 +0530

    First Commit

---
+++ Docker-Image-Sample/src/main/java/com/hello/world/Docker_Image_Sample/App.java
@@ -0,0 +1,13 @@
+package com.hello.world.Docker_Image_Sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(App.class, args);
+    }
+}
---
+++ Docker-Image-Sample/src/main/java/com/hello/world/Docker_Image_Sample/Controller/Demo.java
@@ -0,0 +1,13 @@
+package com.hello.world.Docker_Image_Sample.Controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class Demo {
+	public String helloworld()
+	{
+		return "Hello  World";
+	}
+
+}
